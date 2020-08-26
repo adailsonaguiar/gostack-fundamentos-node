c9d1f7e916ac48d304c84e9dc3dbcdcc7c0022e6
unknown <adailsonacj@live.com>
2020-08-25 22:37:21 -0300

    conclusão do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -16,6 +16,9 @@ class CreateTransactionService {
 
   public execute({ title, value, type }: Request): Transaction {
     // TODO
+    const { income, outcome } = this.transactionsRepository.getBalance();
+    if (outcome + value > income && type === 'outcome')
+      throw Error('O valor de entrada é menor que o valor de saída');
 
     const transaction = this.transactionsRepository.create({
       title,
